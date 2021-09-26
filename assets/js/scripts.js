03788ef4e301e7407c0e4bb165a49442c7914864
giovannicachitarra <ebro330@gmail.com>
2021-09-26 15:55:08 +0200

    Sito def

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,14 @@
-$(document).ready(function(){
-    $('.progetti').slick({
-        autoplay: true
-    });
+$('#apri-menu').click(function() {
+    $('#menu-mobile').addClass('d-flex');
 });
-      
+$('#chiudi-menu').click(function() {
+    $('#menu-mobile').removeClass('d-flex');
+});
+$('#torna-su').click(function() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+$('#bottone-invia').click(function(){
+    $('#invia-messaggio').addClass('nascondi-form');
+    $('.risposta-messaggio').removeClass('risposta-messaggio');
+});
